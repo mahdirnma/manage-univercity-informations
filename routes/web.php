2d1b50b46945e45dc03abbf7344a9739becc3a3a
mahdirnma <rahnamamahdi78@gmail.com>
2024-12-21 19:29:02 +0330

    create admin login process with auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/preLogin', [UserController::class, 'preLogin'])->name('preLogin');
 Route::get('/adminLogin', [UserController::class, 'adminLogin'])->name('admin.login.show');
+Route::post('/adminLogin', [AuthController::class, 'adminLogin'])->name('admin.login');
 Route::get('/studentLogin', [UserController::class, 'studentLogin'])->name('student.login.show');
 Route::get('/professorLogin', [UserController::class, 'professorLogin'])->name('professor.login.show');
+
+Route::get('/', [UserController::class, 'index'])->name('admin.dashboard')->middleware('auth');
 /*Route::get('/', function () {
     return view('welcome');
 });
